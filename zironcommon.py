445a321088a1e8d2f44d116b82ccec91601449ed
Nat Morris <nat@netflix.com>
2020-09-10 14:32:36 +0100

    script to assign a number

--- zironcommon.py
+++ zironcommon.py
@@ -15,11 +15,12 @@ def startup_check_envvars():
             print("Envicronment variable %s must be set" % (varname))
             sys.exit(1)
 
-def account_request(path="", params=None):
+def account_request(path="", params=None, method="GET", data={}):
     account_sid = os.environ.get("ZIRON_ACCOUNT_SID")
     auth_token = os.environ.get("ZIRON_AUTH_TOKEN")
     auth = HTTPBasicAuth(account_sid, auth_token)
-    resp = requests.get("https://api.ziron.com/v1/Accounts/{account_sid}{path}".format(account_sid=account_sid, path=path), auth=auth, params=params)
+    resp = requests.request(method=method, data=data,
+                            url="https://api.ziron.com/v1/Accounts/{account_sid}{path}".format(account_sid=account_sid, path=path), auth=auth, params=params)
     return resp.json()
 
 def account_request_pages(path, params={}):
